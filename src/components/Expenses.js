872f42fb795211fcf6987ac4327cc1cd08ed90f7
rlneto <rlneto@live.com>
2023-08-04 07:10:06 -0300

    css styling no componente que agrupa as despesas

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,8 +1,9 @@
 import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 export default function Expenses(props) {
 	return (
-		<div>
+		<div className="expenses">
 			<ExpenseItem
 				title={props.expenses[0].title}
 				amount={props.expenses[0].amount}
